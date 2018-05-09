85ec23ccb2fe3c2b4da65947935556965cdfb1dc
Hung Nguyen <hnx116@gmail.com>
2018-05-09 12:04:04 +0700

    rewrite and test switch state

--- worker.go
+++ worker.go
@@ -18,9 +18,6 @@ const (
 	REPLAY = "replay"
 )
 
-var closeTimeout = 5 * time.Minute
-var commitTimeout = 30 * time.Minute
-
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -28,8 +25,10 @@ var upgrader = websocket.Upgrader{
 
 type IWorker interface {
 	SetConnection(req *http.Request, res http.ResponseWriter) error
-	Debug()
 	TotalInQueue() int
+	GetState() string
+	SetConfig(config Config)
+	SwitchState(string)
 }
 
 type Id = string
@@ -45,160 +44,341 @@ type Commit struct {
 	Offset int
 }
 
-type Worker struct {
-	sync.RWMutex
+type Config struct {
+	closeTimeout  time.Duration
+	commitTimeout time.Duration
+}
 
-	Id         Id
+type State struct {
+	state string
+	done  chan bool
+}
+
+type Worker struct {
+	sync.Mutex
+
+	id         Id
 	msgChan    <-chan Message
 	deadChan   chan<- Id
 	commitChan chan<- Commit
 
-	ws             *websocket.Conn
-	state          string
-	replayQueue    []*Message
-	commitOffset   int
-	stopChan       chan bool
+	config       *Config
+	ws           *websocket.Conn
+	state        string
+	replayQueue  []*Message
+	commitOffset int
+
 	lastCloseTime  time.Time
 	lastCommitTime time.Time
+
+	stateChan chan State
+
+	stopDeadChecker     chan bool
+	stopSend            chan bool
+	stopChopReplayQueue chan bool
+	stopReplay          bool
 }
 
 func NewWorker(id Id, msgChan <-chan Message, deadChan chan<- Id, commitChan chan<- Commit) IWorker {
-	return &Worker{
-		Id:          id,
+	w := &Worker{
+		id:          id,
 		msgChan:     msgChan,
 		deadChan:    deadChan,
 		commitChan:  commitChan,
 		replayQueue: []*Message{},
-		stopChan:    make(chan bool),
+		config: &Config{
+			closeTimeout:  5 * time.Minute,
+			commitTimeout: 30 * time.Minute,
+		},
+		stopSend:            make(chan bool),
+		stopReplay:          false,
+		stopChopReplayQueue: make(chan bool),
+		stopDeadChecker:     make(chan bool),
+		state:               CLOSED,
+		stateChan:           make(chan State),
 	}
+
+	go w.stateSwitcher()
+	go w.deadChecker()
+
+	return w
 }
 
 func (me *Worker) SetConnection(r *http.Request, w http.ResponseWriter) error {
-	log.Printf("[wsworker: %s] SetConnection", me.Id)
+	log.Printf("[wsworker: %s] SetConnection", me.id)
 
-	ws, err := upgradeHandler(w, r)
-	if err != nil {
-		return err
+	me.Lock()
+	defer me.Unlock()
+
+	// deny
+	if me.state != NORMAL && me.state != CLOSED {
+		log.Printf("[wsworker: %s] SetConnection - not allow", me.id)
+		return errors.New("not allow")
 	}
 
-	go me.wsOnMessage(ws)
+	me.SwitchState(CLOSED) // release resources
+	me.handleWebsocket(w, r)
 
-	if me.state == DEAD {
-		return errors.New("dead")
-	}
-
-	if me.state == REPLAY {
-		return errors.New("replaying")
-	}
-
-	if me.state == NORMAL {
-		me.Stop()
-	}
-
-	// state = "" OR state = CLOSED
-	me.ws = ws
-	me.lastCommitTime = time.Now()
-	me.lastCloseTime = time.Now()
-	me.replay()
-
-	if me.state == NORMAL {
-		go me.deadChecker()
-		go me.chopReplayQueue()
-		go me.send()
-	}
+	me.SwitchState(REPLAY)
+	me.SwitchState(NORMAL)
 
 	return nil
 }
 
-func (me *Worker) send() {
-	log.Printf("[wsworker: %s] send", me.Id)
+func (me *Worker) SwitchState(state string) {
+	log.Printf("[wsworker: %s] SwitchState: %s", me.id, state)
+
+	done := make(chan bool)
+	me.stateChan <- State{state: state, done: done}
+	<-done
+}
+
+func (me *Worker) stateSwitcher() {
+	log.Printf("[wsworker: %s] stateSwitcher", me.id)
+
+	for s := range me.stateChan {
+		state := s.state
+		log.Printf("[wsworker: %s] %s -> %s", me.id, me.state, state)
+
+		if state == me.state {
+			s.done <- true
+			continue
+		}
+
+		switch state {
+		case NORMAL:
+			if me.state == REPLAY {
+				me.state = NORMAL
+				go me.onNormal()
+			}
+
+		case REPLAY:
+			if me.state == CLOSED {
+				me.state = REPLAY
+				me.onReplay()
+			}
+
+		case CLOSED:
+			if me.state == NORMAL || me.state == REPLAY {
+				me.state = CLOSED
+				me.onClosed()
+			}
+
+		case DEAD:
+			if me.state == NORMAL || me.state == CLOSED {
+				me.state = DEAD
+				me.onDead()
+			}
+		}
+
+		s.done <- true
+	}
+}
+
+func (me *Worker) onNormal() {
+	log.Printf("[wsworker: %s] onNormal", me.id)
+
+	go me.chopReplayQueue()
+
 loop:
 	for {
 		select {
 		case msg := <-me.msgChan:
-			me.Lock()
 			me.replayQueue = append(me.replayQueue, &msg)
-			me.Unlock()
 
 			err := me.wsSend(msg.Payload)
 			if err != nil {
-				break loop
+				// use goroutine to fix blocking: SwitchState (wait onNormal - onNormal wait SwitchState)
+				go func() {
+					me.stopSend <- true
+					me.SwitchState(CLOSED)
+				}()
+
+				continue
 			}
 
-			log.Printf("[wsworker: %s] sent: %s", me.Id, string(msg.Payload))
-		case <-me.stopChan:
+			log.Printf("[wsworker: %s] sent: %s", me.id, string(msg.Payload))
+
+		case <-me.stopSend:
 			break loop
 		}
 	}
-
-	me.setState(CLOSED)
 }
 
-func (me *Worker) wsSend(payload []byte) error {
-	return me.ws.WriteMessage(websocket.TextMessage, payload)
-}
+func (me *Worker) onReplay() {
+	log.Printf("[wsworker: %s] onReplay", me.id)
+	log.Printf("[wsworker: %s] total message in queue: %d", me.id, len(me.replayQueue))
 
-func (me *Worker) replay() {
-	log.Printf("[wsworker: %s] replay", me.Id)
-	log.Printf("[wsworker: %s] total message in replay queue: %d", me.Id, len(me.replayQueue))
-	me.setState(REPLAY)
+	me.stopReplay = false
 
 	for _, m := range me.replayQueue {
 		err := me.wsSend(m.Payload)
 		if err != nil {
-			me.setState(CLOSED)
-			return
+			// use goroutine to fix blocking: SwitchState (wait onReplay - onReplay wait SwitchState)
+			go func() {
+				me.SwitchState(CLOSED)
+			}()
+
+			break
+		}
+
+		log.Printf("[wsworker: %s] replayed: %s", me.id, string(m.Payload))
+
+		if me.stopReplay {
+			break
 		}
 	}
+}
 
-	me.setState(NORMAL)
+func (me *Worker) onClosed() {
+	log.Printf("[wsworker: %s] onClosed", me.id)
+
+	me.lastCloseTime = time.Now()
+
+	me.stopReplay = true
+
+	if me.state == NORMAL {
+		me.stopSend <- true
+		me.stopChopReplayQueue <- true
+	}
+}
+
+func (me *Worker) onDead() {
+	log.Printf("[wsworker: %s] onDead", me.id)
+
+	if me.ws != nil {
+		me.ws.Close()
+	}
+
+	if me.state == NORMAL {
+		me.stopSend <- true
+		me.stopChopReplayQueue <- true
+	}
+
+	// use goroutine to fix blocking: deadChecker (wait onDead - onDead wait deadChecker)
+	go func() {
+		me.stopDeadChecker <- true
+	}()
+
+	go func() {
+		me.deadChan <- me.id
+	}()
 }
 
 // no websocket connection for 5 minutes
 // OR dont see "commit" for a long time (30 minutes)
 func (me *Worker) deadChecker() {
-	log.Printf("[wsworker: %s] deadChecker", me.Id)
-	if me.state == DEAD {
+	log.Printf("[wsworker: %s] deadChecker", me.id)
+
+	ticker := time.NewTicker(1 * time.Second)
+loop:
+	for {
+		select {
+		case <-ticker.C:
+			log.Printf("[wsworker: %s] deadChecker checking", me.id)
+			// me.debug()
+
+			if me.state != CLOSED && me.state != NORMAL {
+				return
+			}
+
+			closeDuration := time.Now().Sub(me.lastCloseTime)
+			if me.state == CLOSED && closeDuration > me.config.closeTimeout {
+				log.Printf("[wsworker: %s] close timeout", me.id)
+				me.SwitchState(DEAD)
+			}
+
+			commitDuration := time.Now().Sub(me.lastCommitTime)
+			if me.state == NORMAL && commitDuration > me.config.commitTimeout {
+				log.Printf("[wsworker: %s] commit timeout", me.id)
+				me.SwitchState(DEAD)
+			}
+
+		case <-me.stopDeadChecker:
+			break loop
+		}
+	}
+}
+
+// remove committed messages from replay queue
+func (me *Worker) chopReplayQueue() {
+	log.Printf("[wsworker: %s] chopReplayQueue", me.id)
+
+	ticker := time.NewTicker(1 * time.Second)
+loop:
+	for {
+		select {
+		case <-ticker.C:
+			log.Printf("[wsworker: %s] chopReplayQueue choping", me.id)
+
+			if me.state != NORMAL {
+				return
+			}
+
+			go func() {
+				me.commitChan <- Commit{
+					Id:     me.id,
+					Offset: me.commitOffset,
+				}
+			}()
+
+			newReplayQueue := []*Message{}
+
+			for _, msg := range me.replayQueue {
+				if msg.Offset > me.commitOffset {
+					newReplayQueue = append(newReplayQueue, msg)
+				}
+			}
+
+			me.replayQueue = newReplayQueue
+		case <-me.stopChopReplayQueue:
+			break loop
+		}
+	}
+}
+
+func (me *Worker) TotalInQueue() int {
+	return len(me.replayQueue)
+}
+
+func (me *Worker) GetState() string {
+	return me.state
+}
+
+func (me *Worker) SetConfig(config Config) {
+	me.config = &config
+}
+
+func (me *Worker) handleWebsocket(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("handleWebsocket error: %s", err)
 		return
 	}
 
-	ticker := time.NewTicker(1 * time.Second)
+	me.ws = ws
+	me.lastCommitTime = time.Now()
+	me.lastCloseTime = time.Now()
 
-	for range ticker.C {
-		closeDuration := time.Now().Sub(me.lastCloseTime)
-		if me.ws == nil && closeDuration > closeTimeout {
-			break
-		}
-
-		commitDuration := time.Now().Sub(me.lastCommitTime)
-		if commitDuration > commitTimeout {
-			break
-		}
-	}
-
-	go func() {
-		me.deadChan <- me.Id
-	}()
-
-	me.setState(DEAD)
-	me.Stop()
+	go me.wsOnMessage(ws)
 }
 
-func (me *Worker) waitStopped() {
-	log.Printf("[wsworker: %s] waitStopped", me.Id)
-	for me.state != CLOSED && me.state != DEAD {
-		time.Sleep(100 * time.Millisecond)
+func (me *Worker) wsSend(payload []byte) error {
+	if me.ws == nil {
+		return nil
 	}
+
+	return me.ws.WriteMessage(websocket.TextMessage, payload)
 }
 
 func (me *Worker) wsOnMessage(ws *websocket.Conn) {
-	log.Printf("[wsworker: %s] wsOnMessage", me.Id)
+	log.Printf("[wsworker: %s] wsOnMessage", me.id)
 loop:
 	for {
 		_, p, err := ws.ReadMessage()
 		if err != nil {
-			log.Printf("[wsworker: %s] read message error: %s", me.Id, err)
-			me.Stop()
+			log.Printf("[wsworker: %s] read message error: %s", me.id, err)
+			me.SwitchState(CLOSED)
 			break loop
 		}
 
@@ -207,78 +387,18 @@ loop:
 		if offset > me.commitOffset {
 			me.commitOffset = offset
 		}
+
+		me.lastCommitTime = time.Now()
 	}
 }
 
-func (me *Worker) chopReplayQueue() {
-	log.Printf("[wsworker: %s] chopReplayQueue", me.Id)
-	ticker := time.NewTicker(1 * time.Second)
-
-	for range ticker.C {
-		go func() {
-			me.commitChan <- Commit{
-				Id:     me.Id,
-				Offset: me.commitOffset,
-			}
-		}()
-
-		me.Lock()
-
-		newReplayQueue := []*Message{}
-
-		for _, msg := range me.replayQueue {
-			if msg.Offset > me.commitOffset {
-				newReplayQueue = append(newReplayQueue, msg)
-			}
-		}
-
-		me.replayQueue = newReplayQueue
-		me.Unlock()
-	}
-}
-
-func (me *Worker) Debug() {
+func (me *Worker) debug() {
 	log.Println("-------------------------------------------------")
-	log.Printf("[wsworker: %s] state: %s\n", me.Id, me.state)
-	log.Printf("[wsworker: %s] replayQueue: %v\n", me.Id, me.replayQueue)
-	log.Printf("[wsworker: %s] commitOffset: %d\n", me.Id, me.commitOffset)
-	log.Printf("[wsworker: %s] lastCloseTime: %s\n", me.Id, me.lastCloseTime)
-	log.Printf("[wsworker: %s] lastCommitTime: %s\n", me.Id, me.lastCommitTime)
-}
-
-func (me *Worker) setState(state string) {
-	log.Printf("[wsworker: %s] setState: %s", me.Id, state)
-	if me.state == DEAD {
-		return
-	}
-
-	me.state = state
-}
-
-func (me *Worker) Stop() {
-	log.Printf("[wsworker: %s] Stop", me.Id)
-	me.stopChan <- true
-	me.lastCloseTime = time.Now()
-
-	if me.ws != nil {
-		me.ws.Close()
-		me.ws = nil
-	}
-
-	me.waitStopped()
-}
-
-func (me *Worker) TotalInQueue() int {
-	return len(me.replayQueue)
-}
-
-func upgradeHandler(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return ws, nil
+	log.Printf("[wsworker: %s] state: %s\n", me.id, me.state)
+	log.Printf("[wsworker: %s] replayQueue: %v\n", me.id, me.replayQueue)
+	log.Printf("[wsworker: %s] commitOffset: %d\n", me.id, me.commitOffset)
+	log.Printf("[wsworker: %s] lastCloseTime: %s\n", me.id, me.lastCloseTime)
+	log.Printf("[wsworker: %s] lastCommitTime: %s\n", me.id, me.lastCommitTime)
 }
 
 func strToInt(str string) int {
--- worker_test.go
+++ worker_test.go
@@ -2,32 +2,29 @@ package wsworker
 
 import (
 	"bytes"
-	// "encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-func TestSend(t *testing.T) {
+func TestSendMessage(t *testing.T) {
 	expected := []byte("{\"id\":1}")
 
 	connId := "1"
 	msgChan := make(chan Message, 100)
-	deadChan := make(chan Id)
-	commitChan := make(chan Commit)
-
-	worker := NewWorker(connId, msgChan, deadChan, commitChan)
 
 	msgChan <- Message{
 		Id:      connId,
 		Offset:  1,
 		Payload: expected,
 	}
+
+	worker := NewWorker(connId, msgChan, make(chan Id), make(chan Commit))
 
 	http.HandleFunc("/echo1", func(w http.ResponseWriter, r *http.Request) {
 		worker.SetConnection(r, w)
@@ -43,6 +40,7 @@ func TestSend(t *testing.T) {
 
 	u := url.URL{Scheme: "ws", Host: addr, Path: "/echo1"}
 	c, _, _ := websocket.DefaultDialer.Dial(u.String(), nil)
+	defer c.Close()
 
 	_, got, _ := c.ReadMessage()
 
@@ -51,21 +49,21 @@ func TestSend(t *testing.T) {
 	}
 }
 
-func TestReplay(t *testing.T) {
+func TestReplayMessage(t *testing.T) {
 	expected := []byte("{\"id\":1}")
-
 	connId := "2"
 	msgChan := make(chan Message, 100)
-	deadChan := make(chan Id)
-	commitChan := make(chan Commit)
 
-	worker := NewWorker(connId, msgChan, deadChan, commitChan)
+	msgChan <- Message{
+		Id:      connId,
+		Offset:  1,
+		Payload: expected,
+	}
+
+	worker := NewWorker(connId, msgChan, make(chan Id), make(chan Commit))
 
 	http.HandleFunc("/echo2", func(w http.ResponseWriter, r *http.Request) {
-		err := worker.SetConnection(r, w)
-		if err != nil {
-			fmt.Printf("set connection error: %s\n", err)
-		}
+		worker.SetConnection(r, w)
 	})
 
 	addr := ":9002"
@@ -81,15 +79,8 @@ func TestReplay(t *testing.T) {
 	c1, _, _ := websocket.DefaultDialer.Dial(u.String(), nil)
 	c1.Close()
 
-	time.Sleep(50 * time.Millisecond)
-
-	msgChan <- Message{
-		Id:      connId,
-		Offset:  1,
-		Payload: expected,
-	}
-
 	c2, _, _ := websocket.DefaultDialer.Dial(u.String(), nil)
+	defer c2.Close()
 	_, got, _ := c2.ReadMessage()
 
 	if !bytes.Equal(got, expected) {
@@ -98,22 +89,20 @@ func TestReplay(t *testing.T) {
 }
 
 func TestChopReplayQueue(t *testing.T) {
-	message := []byte("{\"id\":1}")
-
 	connId := "3"
 	msgChan := make(chan Message, 100)
-	deadChan := make(chan Id)
-	commitChan := make(chan Commit)
 
-	worker := NewWorker(connId, msgChan, deadChan, commitChan)
+	total := 2
 
-	for i := 1; i <= 2; i++ {
+	for i := 1; i <= total; i++ {
 		msgChan <- Message{
 			Id:      connId,
 			Offset:  i,
-			Payload: message,
+			Payload: []byte("{\"id\":" + strconv.Itoa(i) + "}"),
 		}
 	}
+
+	worker := NewWorker(connId, msgChan, make(chan Id), make(chan Commit))
 
 	http.HandleFunc("/echo3", func(w http.ResponseWriter, r *http.Request) {
 		worker.SetConnection(r, w)
@@ -129,12 +118,9 @@ func TestChopReplayQueue(t *testing.T) {
 
 	u := url.URL{Scheme: "ws", Host: addr, Path: "/echo3"}
 	c, _, _ := websocket.DefaultDialer.Dial(u.String(), nil)
+	defer c.Close()
 
-	_, msg, _ := c.ReadMessage()
-
-	if bytes.Equal(msg, message) {
-		c.WriteMessage(websocket.TextMessage, []byte("2"))
-	}
+	c.WriteMessage(websocket.TextMessage, []byte(strconv.Itoa(total)))
 
 	time.Sleep(2 * time.Second)
 
@@ -142,5 +128,129 @@ func TestChopReplayQueue(t *testing.T) {
 	got := worker.TotalInQueue()
 	if got != expected {
 		t.Errorf("expected %d, got: %d", expected, got)
+	}
+}
+
+func TestCloseTimeout(t *testing.T) {
+	worker := NewWorker("4", make(chan Message), make(chan Id), make(chan Commit))
+
+	closeTimeout := 2 * time.Second
+	worker.SetConfig(Config{
+		closeTimeout:  closeTimeout,
+		commitTimeout: 30 * time.Minute,
+	})
+
+	http.HandleFunc("/echo4", func(w http.ResponseWriter, r *http.Request) {
+		worker.SetConnection(r, w)
+	})
+
+	addr := ":9004"
+
+	go func() {
+		log.Fatal(http.ListenAndServe(addr, nil))
+	}()
+
+	time.Sleep(50 * time.Millisecond)
+
+	u := url.URL{Scheme: "ws", Host: addr, Path: "/echo4"}
+
+	c, _, _ := websocket.DefaultDialer.Dial(u.String(), nil)
+	c.Close()
+
+	checkerInterval := 1 * time.Second
+	time.Sleep(closeTimeout + checkerInterval)
+
+	expected := DEAD
+	got := worker.GetState()
+
+	if got != expected {
+		t.Errorf("expected %s, got: %s", expected, got)
+	}
+}
+
+func TestCommitTimeout(t *testing.T) {
+	worker := NewWorker("5", make(chan Message), make(chan Id), make(chan Commit))
+
+	commitTimeout := 2 * time.Second
+	worker.SetConfig(Config{
+		closeTimeout:  5 * time.Minute,
+		commitTimeout: commitTimeout,
+	})
+
+	http.HandleFunc("/echo5", func(w http.ResponseWriter, r *http.Request) {
+		worker.SetConnection(r, w)
+	})
+
+	addr := ":9005"
+
+	go func() {
+		log.Fatal(http.ListenAndServe(addr, nil))
+	}()
+
+	time.Sleep(50 * time.Millisecond)
+
+	u := url.URL{Scheme: "ws", Host: addr, Path: "/echo5"}
+
+	c, _, _ := websocket.DefaultDialer.Dial(u.String(), nil)
+	defer c.Close()
+
+	checkerInterval := 1 * time.Second
+	time.Sleep(commitTimeout + checkerInterval)
+
+	expected := DEAD
+	got := worker.GetState()
+
+	if got != expected {
+		t.Errorf("expected %s, got: %s", expected, got)
+	}
+}
+
+func TestNormalState(t *testing.T) {
+	worker := NewWorker("6", make(chan Message), make(chan Id), make(chan Commit))
+	expected := NORMAL
+
+	worker.SwitchState(REPLAY)
+	worker.SwitchState(expected)
+
+	got := worker.GetState()
+
+	if got != expected {
+		t.Errorf("expected %s, got: %s", expected, got)
+	}
+}
+
+func TestReplayState(t *testing.T) {
+	worker := NewWorker("7", make(chan Message), make(chan Id), make(chan Commit))
+
+	expected := REPLAY
+	worker.SwitchState(expected)
+	got := worker.GetState()
+
+	if got != expected {
+		t.Errorf("expected %s, got: %s", expected, got)
+	}
+}
+
+func TestClosedState(t *testing.T) {
+	worker := NewWorker("8", make(chan Message), make(chan Id), make(chan Commit))
+
+	expected := CLOSED
+	worker.SwitchState(expected)
+	got := worker.GetState()
+
+	if got != expected {
+		t.Errorf("expected %s, got: %s", expected, got)
+	}
+}
+
+func TestDeadState(t *testing.T) {
+	worker := NewWorker("9", make(chan Message), make(chan Id), make(chan Commit))
+
+	expected := DEAD
+	worker.SwitchState(expected)
+	got := worker.GetState()
+
+	if got != expected {
+		t.Errorf("expected %s, got: %s", expected, got)
 	}
 }
