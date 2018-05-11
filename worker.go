c08b9901214302c9023b22ca030fcda159b26283
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-05-12 01:29:07 +0700

    fix concurrent me.ws and deadlock

--- worker.go
+++ worker.go
@@ -56,10 +56,8 @@ type Worker struct {
 	deadChan    chan<- Id
 	commitChan  chan<- Commit
 	config      *Config
-	ws          *websocket.Conn
-	state       string
 	replayQueue []*Message
-	newConnChan chan bool
+	newConnChan chan *websocket.Conn
 	machine     fsm.FSM
 }
 
@@ -75,8 +73,7 @@ func NewWorker(id Id, msgChan <-chan Message, deadChan chan<- Id, commitChan cha
 			commitTimeout: 30 * time.Minute,
 		},
 		chopChan:    make(chan int),
-		state:       CLOSED,
-		newConnChan: make(chan bool),
+		newConnChan: make(chan *websocket.Conn),
 		machine:     fsm.New(id),
 	}
 
@@ -97,8 +94,7 @@ func (me *Worker) SetConnection(r *http.Request, w http.ResponseWriter) error {
 	if err != nil {
 		return err
 	}
-	me.ws = ws
-	me.newConnChan <- true
+	me.newConnChan <- ws
 	return nil
 }
 
@@ -111,18 +107,28 @@ func chop(queue []*Message, offset int) []*Message {
 	return nil
 }
 
-func (me *Worker) OnNormal(_ string, _ interface{}) (string, interface{}) {
+func tryRead(c chan bool) bool {
+	select{
+	case c := <- c:
+		return c
+	default:
+	}
+	return false
+}
+
+func (me *Worker) OnNormal(_ string, wsi interface{}) (string, interface{}) {
 	log.Printf("[wsworker: %s] onNormal", me.id)
 	committed, offset := time.Now(), -1
-	ws, currentoffset, closechan := me.ws, 0, make(chan bool)
+	ws := wsi.(*websocket.Conn)
+	currentoffset, closechan := 0, make(chan bool, 2)
 	go func() {
 		for {
 			_, p, err := ws.ReadMessage()
 			if err != nil {
 				log.Printf("[wsworker: %s] read message error: %s", me.id, err)
 				closechan <- true
+				return
 			}
-
 			offset := strToInt(string(p))
 			if offset > currentoffset {
 				me.chopChan <- offset
@@ -131,17 +137,18 @@ func (me *Worker) OnNormal(_ string, _ interface{}) (string, interface{}) {
 		}
 	}()
 
+	defer ws.Close()
 	for {
 		select {
-		case <-me.newConnChan:
-			return ENORMAL, nil
+		case newws := <-me.newConnChan:
+			return ENORMAL, newws
 		case <-closechan:
-			return ECLOSED, nil
+			return ECLOSED, ws
 		case offset = <-me.chopChan:
 		case <-time.After(1 * time.Second):
 			// dead if haven't committed for too long
 			if me.config.commitTimeout < time.Since(committed) {
-				return EDEAD, nil
+				return EDEAD, ws
 			}
 
 			// check for commit offset
@@ -158,50 +165,47 @@ func (me *Worker) OnNormal(_ string, _ interface{}) (string, interface{}) {
 			me.replayQueue = append(me.replayQueue, &msg)
 			if err := me.wsSend(ws, msg.Payload); err != nil {
 				log.Printf("[wsworker: %s] on send error %v", me.id, err)
-				return ECLOSED, nil
+				return ECLOSED, ws
 			}
 		}
 	}
 }
 
-func (me *Worker) OnReplay(_ string, _ interface{}) (string, interface{}) {
+func (me *Worker) OnReplay(_ string, wsi interface{}) (string, interface{}) {
 	log.Printf("[wsworker: %s] onReplay", me.id)
 	log.Printf("[wsworker: %s] total message in queue: %d", me.id, len(me.replayQueue))
-	ws := me.ws
+	ws := wsi.(*websocket.Conn)
 	for _, m := range me.replayQueue {
 		if err := me.wsSend(ws, m.Payload); err != nil {
 			log.Printf("[wsworker: %s] on send error %v", me.id, err)
-			return ECLOSED, nil
+			return ECLOSED, ws
 		}
 
 		log.Printf("[wsworker: %s] replayed: %s", me.id, string(m.Payload))
 	}
-	return ENORMAL, nil
+	return ENORMAL, ws
 }
 
-func (me *Worker) OnClosed(_ string, _ interface{}) (string, interface{}) {
+func (me *Worker) OnClosed(_ string, wsi interface{}) (string, interface{}) {
 	log.Printf("[wsworker: %s] onClosed", me.id)
-
+	ws := wsi.(*websocket.Conn)
 	for {
 		select {
 		case <-time.After(me.config.closeTimeout):
-			return DEAD, nil
-		case <-me.newConnChan:
-			return REPLAY, nil
+			return EDEAD, ws
+		case ws := <-me.newConnChan:
+			return EREPLAY, ws
 		}
 	}
 }
 
-func (me *Worker) OnDead(_ string, _ interface{}) (string, interface{}) {
+func (me *Worker) OnDead(_ string, wsi interface{}) (string, interface{}) {
 	log.Printf("[wsworker: %s] onDead", me.id)
-
 	// release resource
-	if me.ws != nil {
-		me.ws.Close()
-	}
-	me.ws = nil
-	me.replayQueue = nil
+	ws := wsi.(*websocket.Conn)
+	ws.Close()
 
+	me.replayQueue = nil
 	me.deadChan <- me.id
 	me.machine.Stop()
 	return "", nil
@@ -212,7 +216,7 @@ func (me *Worker) TotalInQueue() int {
 }
 
 func (me *Worker) GetState() string {
-	return me.state
+	return me.machine.GetState()
 }
 
 func (me *Worker) SetConfig(config Config) {
@@ -220,15 +224,12 @@ func (me *Worker) SetConfig(config Config) {
 }
 
 func (me *Worker) wsSend(ws *websocket.Conn, payload []byte) error {
-	if ws == nil {
-		return nil
-	}
 	return ws.WriteMessage(websocket.TextMessage, payload)
 }
 
 func (me *Worker) debug() {
 	log.Println("-------------------------------------------------")
-	log.Printf("[wsworker: %s] state: %s\n", me.id, me.state)
+	log.Printf("[wsworker: %s] state: %s\n", me.id, me.machine.GetState())
 	log.Printf("[wsworker: %s] replayQueue: %v\n", me.id, me.replayQueue)
 }
 
